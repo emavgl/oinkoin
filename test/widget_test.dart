f2fc0639879ff7a0f4812f50414ec182c3f08e54
emavgl <emavgl@gmail.com>
2019-12-23 18:29:25 +0100

    shift to new project

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,6 +9,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:piggybank/app.dart';
 
+import 'package:piggybank/main.dart';
+
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
