14eeef690c487294855384acaddf1dd03c14ef5f
sberlato <sberlato@fbk.eu>
2020-04-25 13:02:10 +0200

    removed extra app.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
-import 'app.dart';
+import 'package:piggybank/screens/shell.dart';
 
 void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp( // Material app add the bar
+      title: 'Welcome to PiggyBank',
+      home: Shell(), // The actual content
+      //theme: ThemeData(          // Add the 3 lines from here...
+      // primaryColor: Colors.white,
+      // ),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:piggybank/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
