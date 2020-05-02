fd865a32331999a40343a643b9ed67abd56d85b1
sberlato <sberlato@fbk.eu>
2020-05-02 17:51:55 +0200

    first draft of settings page

---
+++ lib/components/settings-item.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class SettingsItem extends StatelessWidget {
+  final Color iconBackgroundColor;
+  final Icon icon;
+  final String title;
+  final String subtitle;
+  final Function onPressed;
+
+  SettingsItem(
+      {this.iconBackgroundColor,
+      @required this.icon,
+      @required this.title,
+      @required this.onPressed,
+      @required this.subtitle});
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      child: ListTile(
+        leading: CircleAvatar(
+          backgroundColor:
+              iconBackgroundColor == null ? Colors.blue : iconBackgroundColor,
+          child: icon
+        ),
+        title: Text(title),
+        subtitle: Text(subtitle),
+      ),
+      onPressed: onPressed,
+    );
+  }
+}
