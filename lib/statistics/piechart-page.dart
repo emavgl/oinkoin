cbf41c306f09a9d396f969a90f2ec856e042c09c
Emanuele Viglianisi <emanuele.viglianisi@runtastic.com>
2020-07-18 18:45:59 +0200

    wrap pie chart in small card placed on top

--- lib/statistics/piechart-page.dart
+++ lib/statistics/piechart-page.dart
@@ -64,7 +64,7 @@ class PieChartPageState extends State<PieChartPage> {
           palette[i].shadeDefault,
         domainFn: (LinearRecord records, _) => records.category,
         measureFn: (LinearRecord records, _) => records.value,
-        labelAccessorFn: (LinearRecord row, _) => '${row.category}: ${'${row.value.toStringAsFixed(2)}%'}',
+        labelAccessorFn: (LinearRecord row, _) => row.category,
         data: data,
       )
     ];
@@ -82,54 +82,64 @@ class PieChartPageState extends State<PieChartPage> {
     seriesList = _prepareData(widget.records);
   }
 
+  Widget _buildCardPieChart() {
+    return Container(
+        padding: EdgeInsets.all(10),
+        height: 200,
+        child: new Card(
+            child: new Container(
+              padding: EdgeInsets.all(10),
+              child: new charts.PieChart(
+                  seriesList,
+                  animate: animate,
+
+                  // Add the legend behavior to the chart to turn on legends.
+                  // This example shows how to optionally show measure and provide a custom
+                  // formatter.
+                  defaultRenderer: new charts.ArcRendererConfig(arcWidth: 70, arcRendererDecorators: [
+                    new charts.ArcLabelDecorator(
+                        labelPosition: charts.ArcLabelPosition.outside)
+                  ]),
+                  behaviors: [
+                    new charts.DatumLegend(
+                      outsideJustification: charts.OutsideJustification.middleDrawArea,
+                      // Positions for "start" and "end" will be left and right respectively
+                      // for widgets with a build context that has directionality ltr.
+                      // For rtl, "start" and "end" will be right and left respectively.
+                      // Since this example has directionality of ltr, the legend is
+                      // positioned on the right side of the chart.
+                      position: charts.BehaviorPosition.end,
+                      // By default, if the position of the chart is on the left or right of
+                      // the chart, [horizontalFirst] is set to false. This means that the
+                      // legend entries will grow as new rows first instead of a new column.
+                      horizontalFirst: false,
+                      // This defines the padding around each legend entry.
+                      cellPadding: new EdgeInsets.only(right: 4.0, bottom: 4.0),
+                      // Set [showMeasures] to true to display measures in series legend.
+                      showMeasures: true,
+                      // Configure the measure value to be shown by default in the legend.
+                      legendDefaultMeasure: charts.LegendDefaultMeasure.firstValue,
+                      // Optionally provide a measure formatter to format the measure value.
+                      // If none is specified the value is formatted as a decimal.
+                      measureFormatter: (num value) {
+                        return value == null ? '-' : '${value.toStringAsFixed(2)}%';
+                      },
+                    ),
+                  ],
+                )              
+            )
+        )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Material(
         type: MaterialType.transparency,
-        child: new Container(
-            height: MediaQuery.of(context).size.height / 2,
-            width: MediaQuery.of(context).size.width / 2,
-            decoration: new BoxDecoration(color: Colors.red),
-            child: new charts.PieChart(
-              seriesList,
-              animate: animate,
-
-              // Add the legend behavior to the chart to turn on legends.
-              // This example shows how to optionally show measure and provide a custom
-              // formatter.
-              defaultRenderer: new charts.ArcRendererConfig(arcWidth: 70, arcRendererDecorators: [
-                new charts.ArcLabelDecorator(
-                    labelPosition: charts.ArcLabelPosition.outside)
-              ]),
-              behaviors: [
-                new charts.DatumLegend(
-                  outsideJustification: charts.OutsideJustification.middleDrawArea,
-                  // Positions for "start" and "end" will be left and right respectively
-                  // for widgets with a build context that has directionality ltr.
-                  // For rtl, "start" and "end" will be right and left respectively.
-                  // Since this example has directionality of ltr, the legend is
-                  // positioned on the right side of the chart.
-                  position: charts.BehaviorPosition.end,
-                  // By default, if the position of the chart is on the left or right of
-                  // the chart, [horizontalFirst] is set to false. This means that the
-                  // legend entries will grow as new rows first instead of a new column.
-                  horizontalFirst: false,
-                  // This defines the padding around each legend entry.
-                  cellPadding: new EdgeInsets.only(right: 4.0, bottom: 4.0),
-                  // Set [showMeasures] to true to display measures in series legend.
-                  showMeasures: true,
-                  // Configure the measure value to be shown by default in the legend.
-                  legendDefaultMeasure: charts.LegendDefaultMeasure.firstValue,
-                  // Optionally provide a measure formatter to format the measure value.
-                  // If none is specified the value is formatted as a decimal.
-                  measureFormatter: (num value) {
-                    return value == null ? '-' : '${value.toStringAsFixed(2)}%';
-                  },
-                ),
-              ],
-            )
+        child: new Align(
+            alignment: Alignment.topCenter,
+            child: _buildCardPieChart()
         )
     );
-
   }
 }
