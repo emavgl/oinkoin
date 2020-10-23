3c99154f68cabbfbacaa05fd8ecb20f2932c26a7
Emanuele Viglianisi <emanuele.viglianisi@runtastic.com>
2020-10-23 16:53:17 +0200

    update flutter and libraries and fix build script

--- build.sh
+++ build.sh
@@ -5,14 +5,14 @@ rm -rf ~/Desktop/tmp_build
 mkdir ~/Desktop/tmp_build/
 
 # build free version
-sed -i "" 's/Oinkoin Pro/Oinkoin/' android/app/src/AndroidManifest.xml
+sed -i "" 's/Oinkoin Pro/Oinkoin/' ./android/app/src/main/AndroidManifest.xml
 sed -i "" 's/com.github.emavgl.piggybankpro/com.github.emavgl.piggybank/' android/app/build.gradle
 sed -i "" 's/isPremium = true/isPremium = false/' ./lib/services/service-config.dart
 flutter build apk --obfuscate --split-debug-info=./build-debug-files --split-per-abi
 cp -r build/app/outputs/flutter-apk/ ~/Desktop/tmp_build/free-apk
 
 # build premium version
-sed -i "" 's/Oinkoin/Oinkoin Pro/' android/app/src/AndroidManifest.xml
+sed -i "" 's/Oinkoin/Oinkoin Pro/' ./android/app/src/main/AndroidManifest.xml
 sed -i "" 's/com.github.emavgl.piggybank/com.github.emavgl.piggybankpro/' ./android/app/build.gradle
 sed -i "" 's/isPremium = false/isPremium = true/'  ./lib/services/service-config.dart
 flutter build apk --obfuscate --split-debug-info=./build-debug-files --split-per-abi
