ced37aa3adf2aa64626905510dcda8d6965539cc
emavgl <emavgl@gmail.com>
2024-06-19 23:01:01 +0200

    Update gradle files and others

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -3,6 +3,7 @@
 export "FLUTTER_ROOT=C:\development\oinkoin\submodules\flutter"
 export "FLUTTER_APPLICATION_PATH=C:\development\oinkoin"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.62"
 export "FLUTTER_BUILD_NUMBER=6055"
