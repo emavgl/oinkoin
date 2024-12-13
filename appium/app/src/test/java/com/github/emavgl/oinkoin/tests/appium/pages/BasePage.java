c520ee3f7403fc7f255fb3766bdc596ebd3efa0c
raulsorrentino <47747453+raulsorrentino@users.noreply.github.com>
2024-12-13 23:41:01 +0100

    fix: initialize PageFactory with AppiumFieldDecorator
    Updated BasePage to use `new AppiumFieldDecorator(driver)` for proper initialization
    of mobile element annotations (e.g., `@AndroidFindBy`). This ensures compatibility
    with Appium locators and resolves issues with locating elements using accessibility IDs.

--- appium/app/src/test/java/com/github/emavgl/oinkoin/tests/appium/pages/BasePage.java
+++ appium/app/src/test/java/com/github/emavgl/oinkoin/tests/appium/pages/BasePage.java
@@ -1,6 +1,7 @@
 package com.github.emavgl.oinkoin.tests.appium.pages;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -25,7 +26,7 @@ public abstract class BasePage {
 
     public BasePage(AppiumDriver driver) {
         this.driver = driver;
-        PageFactory.initElements(driver, this);
+        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
     }
 
     public boolean isDisplayed(WebElement webElement) {
